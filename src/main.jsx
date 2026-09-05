035ace5014c06a9c96f6d19232da14b4177360c1
unknown <padmakarnarwade05@gmail.com>
2026-09-05 14:19:38 +0530

    in main Import path case change

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ModalWinProvider } from "./components/common/ModalWin";
-import NotificationProvider from "./components/common/Notification/NotificationProvider";
+import NotificationProvider from "./components/common/notification/NotificationProvider";
 import AppRoutes from "./routes/Routes";
 import ThemeManager from "./components/common/ThemeManager";
 import store from "../src/store/store";
